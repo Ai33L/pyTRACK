2e5654d6febd7c80e0e25f5d61adb848296a10ae
Abel <abels2000@gmail.com>
2026-02-11 18:59:48 +0000

    added fortran tools

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class BuildTRACK(build_py):
 
 setup(
     name="pyTRACK",
-    version="0.2.0",
+    version="0.3.0",
     description="Python-wrapped implementation of the TRACK software",
     packages=find_packages(),
     cmdclass={"build_py": BuildTRACK},
