88535b2a4c6961db2164665720658fca05346a98
Abel <abels2000@gmail.com>
2026-02-11 15:57:39 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,5 +17,8 @@ autosummary_generate = True
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'  # nicer sidebar navigation
+html_theme = "furo"
+html_theme_options = {
+    "navigation_with_keys": True,
+}
 html_static_path = ['_static']
