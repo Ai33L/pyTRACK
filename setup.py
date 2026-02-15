4ce0ace0199d00df561f17adbbf964ce1f0b3ab7
Abel <abels2000@gmail.com>
2026-02-15 01:13:27 +0000

    manylinux try

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_py import build_py
-import subprocess, os, shutil, tarfile, glob
+import subprocess, os, shutil, glob, tarfile
 
 TRACK_TARBALL = "TRACK-1.5.2.tar.bz2"
 TRACK_DIR = "TRACK-1.5.2"
@@ -8,22 +8,30 @@ TRACK_DIR = "TRACK-1.5.2"
 class BuildTRACK(build_py):
     def run(self):
         self.build_track()
-        self.build_extensions()
+        self.register_extensions()
         super().run()
 
     def build_track(self):
         root = os.path.abspath(os.path.dirname(__file__))
-        track_src = os.path.join(root, "src", TRACK_DIR)
+        src_dir = os.path.join(root, "src")
+
+        # Extract tarball if needed
+        track_src = os.path.join(src_dir, TRACK_DIR)
         if not os.path.isdir(track_src):
-            with tarfile.open(os.path.join(root, "src", TRACK_TARBALL)) as tar:
-                tar.extractall(os.path.join(root, "src"))
-        subprocess.check_call(["make", "libtrack"], cwd=track_src)
+            with tarfile.open(os.path.join(src_dir, TRACK_TARBALL)) as tar:
+                tar.extractall(src_dir)
+
+        # Build shared libraries in the src folder where Makefile lives
+        subprocess.check_call(["make"], cwd=src_dir)
+
         lib_dst = os.path.join(root, "pyTRACK", "_lib")
         os.makedirs(lib_dst, exist_ok=True)
-        for so_file in ["libtrack.so", "libtrackutils.so"]:
-            shutil.copy(os.path.join(track_src, so_file), lib_dst)
 
-    def build_extensions(self):
+        for so_file in ["libtrack.so", "libtrackutils.so"]:
+            src = os.path.join(src_dir, so_file)
+            shutil.copy(src, lib_dst)
+
+    def register_extensions(self):
         so_files = glob.glob(os.path.join("pyTRACK", "_lib", "*.so"))
         for so in so_files:
             module_name = "pyTRACK._lib." + os.path.splitext(os.path.basename(so))[0]
