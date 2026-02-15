45d9de5968c4c6928bc5ec55633e9220e102bbcc
Abel <abels2000@gmail.com>
2026-02-15 00:46:34 +0000

    manylinux try

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 from setuptools.command.build_py import build_py
 import subprocess
 import os
 import shutil
 import tarfile
+import glob
 
 TRACK_TARBALL = "TRACK-1.5.2.tar.bz2"
 TRACK_DIR = "TRACK-1.5.2"
@@ -24,19 +25,27 @@ class BuildTRACK(build_py):
             with tarfile.open(os.path.join(src_dir, TRACK_TARBALL)) as tar:
                 tar.extractall(src_dir)
 
-        # Run Makefile to build libtrack.so
         print("Running make libtrack...")
         subprocess.check_call(["make", "libtrack"], cwd=src_dir)
 
-        # Copy built .so into package _lib folder
-        lib_src = os.path.join(track_src, "libtrack.so")
         lib_dst = os.path.join(root, "pyTRACK", "_lib")
         os.makedirs(lib_dst, exist_ok=True)
-        shutil.copy(lib_src, lib_dst)
-        lib_src = os.path.join(track_src, "libtrackutils.so")
-        lib_dst = os.path.join(root, "pyTRACK", "_lib")
-        shutil.copy(lib_src, lib_dst)
-        print(f"libtrack.so and libtrackutils.so copied to {lib_dst}")
+
+        for so_file in ["libtrack.so", "libtrackutils.so"]:
+            src = os.path.join(track_src, so_file)
+            shutil.copy(src, lib_dst)
+            print(f"{so_file} copied to {lib_dst}")
+
+so_files = glob.glob(os.path.join("pyTRACK", "_lib", "*.so"))
+ext_modules = []
+for so in so_files:
+    module_name = "pyTRACK._lib." + os.path.splitext(os.path.basename(so))[0]
+    ext_modules.append(
+        Extension(
+            module_name,
+            sources=[],
+        )
+    )
 
 setup(
     name="pyTRACK",
@@ -45,4 +54,5 @@ setup(
     packages=find_packages(),
     cmdclass={"build_py": BuildTRACK},
     include_package_data=True,
+    ext_modules=ext_modules,
 )
