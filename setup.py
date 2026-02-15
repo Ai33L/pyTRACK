e3a81d0641b8523f062fecce80e4d4c59ee69da7
Abel <abels2000@gmail.com>
2026-02-15 01:19:57 +0000

    manylinux try

--- setup.py
+++ setup.py
@@ -1,50 +1,44 @@
 from setuptools import setup, find_packages, Extension
-from setuptools.command.build_py import build_py
-import subprocess, os, shutil, glob, tarfile
+from setuptools.command.build_ext import build_ext
+import subprocess, os, shutil, tarfile, glob
 
 TRACK_TARBALL = "TRACK-1.5.2.tar.bz2"
 TRACK_DIR = "TRACK-1.5.2"
 
-class BuildTRACK(build_py):
+class BuildTrackExt(build_ext):
     def run(self):
-        self.build_track()
-        self.register_extensions()
-        super().run()
-
-    def build_track(self):
         root = os.path.abspath(os.path.dirname(__file__))
         src_dir = os.path.join(root, "src")
-
-        # Extract tarball if needed
         track_src = os.path.join(src_dir, TRACK_DIR)
+
         if not os.path.isdir(track_src):
             with tarfile.open(os.path.join(src_dir, TRACK_TARBALL)) as tar:
                 tar.extractall(src_dir)
 
-        # Build shared libraries in the src folder where Makefile lives
+        # Build shared libs via Makefile
         subprocess.check_call(["make"], cwd=src_dir)
 
         lib_dst = os.path.join(root, "pyTRACK", "_lib")
         os.makedirs(lib_dst, exist_ok=True)
 
         for so_file in ["libtrack.so", "libtrackutils.so"]:
-            src = os.path.join(src_dir, so_file)
-            shutil.copy(src, lib_dst)
+            shutil.copy(os.path.join(src_dir, so_file), lib_dst)
 
-    def register_extensions(self):
-        so_files = glob.glob(os.path.join("pyTRACK", "_lib", "*.so"))
-        for so in so_files:
-            module_name = "pyTRACK._lib." + os.path.splitext(os.path.basename(so))[0]
-            ext = Extension(module_name, sources=[])
-            if not hasattr(self.distribution, "ext_modules") or self.distribution.ext_modules is None:
-                self.distribution.ext_modules = []
-            self.distribution.ext_modules.append(ext)
+        # Now call default build_ext which will compile the dummy C file
+        super().run()
+
+extensions = [
+    Extension(
+        "pyTRACK._lib._dummy_extension",
+        sources=["pyTRACK/_lib/_dummy_extension.c"],
+    )
+]
 
 setup(
     name="pyTRACK",
     version="0.4.1",
-    description="Python-wrapped implementation of the TRACK software",
     packages=find_packages(),
-    cmdclass={"build_py": BuildTRACK},
+    ext_modules=extensions,
+    cmdclass={"build_ext": BuildTrackExt},
     include_package_data=True,
 )
